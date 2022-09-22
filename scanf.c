8c27b9cf72b07d8f614ca2d8e5fac71f953868a8
Julius-Okeyo <jaykopiyo@gmail.com>
2022-09-22 15:21:59 +0300

    Created scanf.c

---
+++ scanf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    char First_name[] = "";
+    char Surname[] = "";
+    printf("Enter first name: ");
+    scanf("%s", &First_name);
+    printf("Enter surname: ");
+    scanf("%s", &Surname);
+    printf("Welcome to C programming %s %s",First_name,Surname);
+
+
+
+}
